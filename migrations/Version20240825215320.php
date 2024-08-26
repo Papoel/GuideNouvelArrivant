041dd8b9f32151827731326702466abeb5694f20
Papoel <pascal.briffard@gmail.com>
2024-08-26 22:27:03 +0200

    🔧 CHORE: cache php-cs-fixer

--- migrations/Version20240825215320.php
+++ migrations/Version20240825215320.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240820145906 extends AbstractMigration
+final class Version20240825215320 extends AbstractMigration
 {
     public function getDescription(): string
     {
