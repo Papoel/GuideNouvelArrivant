e41e6724167b162dd7600aece83358d02b2e0d59
Papoel <pascal.briffard@gmail.com>
2024-11-18 22:49:59 +0100

    📦 DEPS: Ajout de PHPFlasher

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Flasher\Symfony\FlasherSymfonyBundle::class => ['all' => true],
+    Flasher\SweetAlert\Symfony\FlasherSweetAlertSymfonyBundle::class => ['all' => true],
 ];
