fd48b95602045f01cde670311044a7ae439b1333
Papoel <pascal.briffard@gmail.com>
2025-02-16 13:55:58 +0100

    ✨ FEAT: design ribbon-end

--- SendEmail.php
+++ SendEmail.php
@@ -2,11 +2,10 @@
 
 declare(strict_types=1);
 
-// chemin vers le fichier console
+// Chemin vers le fichier console de Symfony
 $console = __DIR__ . '/bin/console';
 
 // Commande à exécuter
 $command = 'php ' . $console . ' messenger:consume async';
 
-// Exécuter la commande
-exec($command);
+exec(command: $command);
