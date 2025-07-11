75d74f33a099012e7b4d40f7df0af47220d24ab6
Papoel <pascal.briffard@gmail.com>
2025-07-11 21:05:03 +0200

    📦 DEPS: symfony/stimulus-bundle

--- importmap.php
+++ importmap.php
@@ -38,4 +38,8 @@ return [
     '@symfony/ux-live-component' => [
         'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
     ],
+    'bootstrap-icons/font/bootstrap-icons.min.css' => [
+        'version' => '1.13.1',
+        'type' => 'css',
+    ],
 ];
