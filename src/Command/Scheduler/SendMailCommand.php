fd8f9ff81b8ed42b6a5529a548bbaf47f4db0998
Papoel <pascal.briffard@gmail.com>
2025-09-20 14:22:08 +0200

    RabbitMQ initialisation

--- src/Command/Scheduler/SendMailCommand.php
+++ src/Command/Scheduler/SendMailCommand.php
@@ -2,22 +2,13 @@
 
 namespace App\Command\Scheduler;
 
-use App\Repository\LogbookRepository;
-use App\Services\Logbook\LogbookProgressService;
-use App\Services\Mail\MentorReminderEmailService;
-use Symfony\Component\Console\Attribute\AsCommand;
+use App\Message\SendReminderEmails;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Mime\Part\DataPart;
-use Symfony\Component\Mime\Part\File;
 use Symfony\Component\Scheduler\Attribute\AsCronTask;
-use Twig\Environment;
 
 #[AsCommand(
     name: 'app:send-mail',
@@ -33,12 +24,7 @@ use Twig\Environment;
 class SendMailCommand extends Command
 {
     public function __construct(
-        private readonly LogbookProgressService $logbookProgressService,
-        private readonly LogbookRepository $logbookRepository,
-        private readonly MentorReminderEmailService $reminderService,
-        private readonly MailerInterface $mailer,
-        private readonly Environment $twig,
-        private readonly ParameterBagInterface $parameterBag,
+        private readonly MessageBusInterface $bus
     ) {
         parent::__construct();
     }
@@ -50,71 +36,9 @@ class SendMailCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
+        $this->bus->dispatch(new SendReminderEmails(triggeredAt: new \DateTimeImmutable()));
+        $output->writeln('📨 Message "SendReminderEmails" dispatché vers RabbitMQ');
 
-        $io->title('Envoi des emails de rappel aux mentors');
-
-        try {
-            $logbooks = $this->logbookRepository->findAllWithOwnerAndMentor();
-            $mentorsWithPending = $this->logbookProgressService->getMentorsWithPendingValidations($logbooks);
-
-            $sentEmails = 0;
-
-            // Chemin du logo
-            $projectDirParameter = $this->parameterBag->get('kernel.project_dir');
-            if (!is_string($projectDirParameter)) {
-                throw new \RuntimeException(message: 'kernel.project_dir doit être une chaîne de caractères');
-            }
-            $logoPath = $projectDirParameter . '/public/images/logos/edf.png';
-
-            $io->progressStart(max: count(value: $mentorsWithPending['mentors_with_pending_modules']));
-
-            foreach ($mentorsWithPending['mentors_with_pending_modules'] as $mentorData) {
-                $reminderData = $this->reminderService->prepareReminderData($mentorData);
-
-                // Générer un Content-ID pour le logo
-                $contentId = 'logo@gna-edf.fr';
-                $reminderData['logo_cid'] = $contentId;
-
-                // Rendre le template avec les données
-                $htmlContent = $this->twig->render(name: 'mail/mentorReminderEmailTemplate.html.twig', context: $reminderData);
-
-                // Créer l'email
-                $emailSubject = $reminderData['subject'];
-
-                $email = (new Email())
-                    ->from('noreply@gna-edf.fr')
-                    ->to($mentorData['mentor_email'])
-                    ->addBcc('bridevproject@gmail.com')
-                    ->subject($emailSubject)
-                    ->html($htmlContent);
-
-                // Intégrer le logo
-                if (file_exists($logoPath)) {
-                    $email->addPart((new DataPart(new File($logoPath), 'logo', 'image/png'))->asInline());
-
-                    // Mettre à jour le template pour utiliser le Content-ID simple
-                    $reminderData['logo_cid'] = 'logo';
-                    $htmlContent = $this->twig->render('mail/mentorReminderEmailTemplate.html.twig', $reminderData);
-                    $email->html($htmlContent);
-                }
-
-                $this->mailer->send($email);
-                $sentEmails++;
-
-                $io->progressAdvance();
-            }
-
-            $io->progressFinish();
-            $io->success(sprintf('%d rappels de validation envoyé(s) par email.', $sentEmails));
-
-            return Command::SUCCESS;
-        } catch (\Exception $e) {
-            $io->error('Erreur lors de l\'envoi des emails : ' . $e->getMessage());
-            return Command::FAILURE;
-        } catch (TransportExceptionInterface $e) {
-            // Logger l'erreur
-            $io->error($e->getMessage());
-        }
+        return Command::SUCCESS;
     }
 }
